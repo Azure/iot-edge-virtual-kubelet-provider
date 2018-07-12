21ed67e42e610f5cfd0845ae280a1f939ac22221
Venkat Yalla <veyalla@microsoft.com>
2018-07-11 23:16:14 -0700

    add missing include directive

--- src/Microsoft.Azure.VirtualKubelet.Edge.Provider/EdgeModule.cs
+++ src/Microsoft.Azure.VirtualKubelet.Edge.Provider/EdgeModule.cs
@@ -1,6 +1,8 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System.Collections.Generic;
+
 namespace Microsoft.Azure.VirtualKubelet.Edge.Provider
 {
     using Newtonsoft.Json;
